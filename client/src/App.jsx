0f9b50399d585e35ba8880faf030718144350c2d
MirzaSameer301 <sameerijaz301@gmail.com>
2025-02-23 09:20:14 +0500

    auth layout and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AuthLayout from "./components/auth/AuthLayout";
+import AuthLogin from "./pages/auth/AuthLogin";
+import AuthRegister from "./pages/auth/AuthRegister";
 
 export const App = () => {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
-        
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<AuthLogin />} />
+          <Route path="register" element={<AuthRegister />} />
+        </Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
