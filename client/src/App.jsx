5401808dc45b8051b6a1cbc3570cf4ddef4752e5
MirzaSameer301 <sameerijaz301@gmail.com>
2025-02-23 15:35:41 +0500

    shopping layout and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import AuthLayout from "./components/auth/AuthLayout";
 import AuthLogin from "./pages/auth/AuthLogin";
 import AuthRegister from "./pages/auth/AuthRegister";
-import AdminLayout from './components/admin-view/AdminLayout'
+import AdminLayout from "./components/admin-view/AdminLayout";
 import AdminDashboard from "./pages/admin-view/AdminDashboard";
 import AdminProducts from "./pages/admin-view/AdminProducts";
 import AdminOrders from "./pages/admin-view/AdminOrders";
 import AdminFeatures from "./pages/admin-view/AdminFeatures";
+import ShoppingLayout from "./components/shopping-view/ShoppingLayout";
+import NotFoundPage from "./pages/NotFoundPage";
+import { ShoppingHome } from "./pages/shopping-view/ShoppingHome";
+import ShoppingCheckout from "./pages/shopping-view/ShoppingCheckout";
+import ShoppingAccount from "./pages/shopping-view/ShoppingAccount";
+import ShoppingListings from "./pages/shopping-view/ShoppingListings";
 
 export const App = () => {
   return (
@@ -17,12 +23,19 @@ export const App = () => {
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
-        <Route path="/admin" element={<AdminLayout/>}>
-          <Route path="dashboard" element={< AdminDashboard />} />
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
-          <Route path="orders" element={< AdminOrders/>} />
+          <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
         </Route>
+        <Route path="/shop" element={<ShoppingLayout />}>
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="listings" element={<ShoppingListings />} />
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="account" element={<ShoppingAccount />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
---
+++ client/src/pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>Page Doesn't Exist </div>
+  )
+}
+
+export default NotFoundPage
