6c731b1c493bed5052b6c154a74e6f2074c582c5
MirzaSameer301 <sameerijaz301@gmail.com>
2025-02-23 08:33:24 +0500

    basic client side setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+
+export const App = () => {
+  return (
+    <div className="flex flex-col overflow-hidden bg-white">
+      <Routes>
+        
+      </Routes>
+    </div>
+  )
+}
