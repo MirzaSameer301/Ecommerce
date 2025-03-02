7f0ac42ad0d4274dcc2a0a7d85e2026cee47e9f9
MirzaSameer301 <sameerijaz301@gmail.com>
2025-03-02 16:18:35 +0500

    crud operations for admin products at server side

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,4 +1,5 @@
 import { uploadImageUtil } from "../helpers/cloudinary.js";
+import Product from "../models/Product.js";
 
 export const handleImageUpload = async (req, res) => {
   try {
@@ -15,6 +16,127 @@ export const handleImageUpload = async (req, res) => {
     res.json({
       success: false,
       message: "Error occured",
+    });
+  }
+};
+
+export const addProduct = async (req, res) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+
+    const newProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    });
+    await newProduct.save();
+    res.status(201).json({
+      success: true,
+      data: newProduct,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      success: false,
+      message: "Failed to add product",
+    });
+  }
+};
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const productsList = await Product.find({});
+    res.status(200).json({
+      success: true,
+      data: productsList,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      success: false,
+      message: "Error Occured whlie fetching products",
+    });
+  }
+};
+
+export const editProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+
+    const findProduct = await Product.findById(id);
+    if (!findProduct) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    findProduct.image = image || findProduct.image;
+    findProduct.title = title || findProduct.title;
+    findProduct.description = description || findProduct.description;
+    findProduct.category = category || findProduct.category;
+    findProduct.brand = brand || findProduct.brand;
+    findProduct.price = price || findProduct.price;
+    findProduct.salePrice = salePrice || findProduct.salePrice;
+    findProduct.totalStock = totalStock || findProduct.totalStock;
+
+    await findProduct.save();
+    res.status(200).json({
+      success: true,
+      data: findProduct,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      success: false,
+      message: "Failed to update product",
+    });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      success: false,
+      message: "Failed to add product",
     });
   }
 };
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,9 +1,19 @@
-import express from 'express'
-import { handleImageUpload } from '../controllers/productController.js';
-import { upload } from '../helpers/cloudinary.js';
+import express from "express";
+import {
+  addProduct,
+  deleteProduct,
+  editProduct,
+  getAllProducts,
+  handleImageUpload,
+} from "../controllers/productController.js";
+import { upload } from "../helpers/cloudinary.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.post('/upload-image',upload.single("my_file"),handleImageUpload);
+router.post("/upload-image", upload.single("my_file"), handleImageUpload);
+router.post("/add", addProduct);
+router.get("/get", getAllProducts);
+router.put("/edit/:id", editProduct);
+router.delete("/delete/:id", deleteProduct);
 
 export default router;
