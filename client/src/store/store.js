8b3ef0ff47ae2c5d6c858837d643f8e7bb5722da
MirzaSameer301 <sameerijaz301@gmail.com>
2025-03-09 11:29:04 +0500

    created cart slice and asyncthunk function for cart crud operations

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice.js";
 import adminProductSlice from './adminProductSlice.js'
 import shopProductSlice from './shopProductSlice.js'
+import cartSlice from './cartSlice.js'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts:adminProductSlice,
-    shopProducts:shopProductSlice
+    shopProducts:shopProductSlice,
+    cartItems:cartSlice
   },
 });
 
